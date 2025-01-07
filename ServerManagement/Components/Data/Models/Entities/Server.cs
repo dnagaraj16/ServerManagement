7b672cbd5cf2d41e9037a6999e938fc54ac7fa15
tarun <tarun@DESKTOP-STA2C2O>
2025-01-07 20:27:08 +0530

    Add project files.

---
+++ ServerManagement/Components/Data/Models/Entities/Server.cs
@@ -0,0 +1,21 @@
+﻿namespace ServerManagement.Components.Data.Models.Entities
+{
+    public class Server
+    {
+
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber == 0 ? false : true;
+        }
+
+
+        public int Id { get; set; }
+        public bool IsOnline { get; set; }
+
+        public string? Name { get; set; }
+        public string? City { get; set; }
+
+    }
+}
---
+++ ServerManagement/Components/Data/Repositories/CitiesRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace ServerManagement.Components.Data.Repositories
+{
+    public static class CitiesRepository
+    {
+
+        private static readonly List<string> Cities = new List<string>()
+        {
+            "Toronto",
+            "Bangalore",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halfix"
+
+        };
+
+        public static List<string> GetCities() { return Cities; }
+
+
+    }
+}
